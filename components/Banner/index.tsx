34b58a869e55c72b2430cb0516ceec4e7211ca7d
seonghoon kim <pole@1024corp.com>
2022-10-26 11:16:07 +0900

    play 버튼 수정

--- components/Banner/index.tsx
+++ components/Banner/index.tsx
@@ -33,12 +33,12 @@ function Banner({netflixOriginals}:Props) {
                 <p className="max-w-xs text-shadow-md text-xs md:max-w-lg lg:max-w-2xl lg:text-2xl">{movie?.overview}</p>
 
             <div className='flex space-x-3'>
-                <button className="bannerButton bg-white text-black"> <FaPlay className="h-4 w-4 text-black md:h-7 md:w-7" />
-                    Play</button>
-                <button className="bannerButton bg-[gray]/70" onClick={() =>{
+                <button className="bannerButton bg-white text-black "onClick={() =>{
                     setCurrentMovie(movie)
                     setShowModal(true)
-                }}>More Info<InformationCircleIcon className='h-5 w-5 md:h-8 md:w-8' /></button>
+                }}> <FaPlay className="h-4 w-4 text-black md:h-7 md:w-7" />
+                    Play</button>
+                <button className="bannerButton bg-[gray]/70" >More Info<InformationCircleIcon className='h-5 w-5 md:h-8 md:w-8' /></button>
             </div>
         </div>
     )
